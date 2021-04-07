b8a8d9af90bad9a680a9bb14c7075048164037f4
seop <iou000@naver.com>
2021-04-08 02:32:36 +0900

    끝~

---
+++ main.js
@@ -0,0 +1,8 @@
+const moreBtn = document.querySelector('.video_info .metadata .moreBtn');
+const title = document.querySelector('.video_info .metadata .title');
+
+
+moreBtn.addEventListener('click', () => {
+    moreBtn.classList.toggle('clicked');
+    title.classList.toggle('clamp');
+});
